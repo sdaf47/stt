d20075014822d5fe2bb5a3cb5654a9817b386e7e
sdaf47 <sdaf47@yandex.ru>
2017-08-31 15:40:07 +0300

    reflection: get by id

--- lib/db/server.go
+++ lib/db/server.go
@@ -47,3 +47,34 @@ func (s *Server) AddEntity(e Entity) (int) {
 
 	return int(id)
 }
+
+func (s *Server) GetEntity(m Entity, id int) (interface{}) {
+	valRef := reflect.ValueOf(m).Elem()
+	typeRef := reflect.TypeOf(m).Elem()
+
+	rv := reflect.New(typeRef)
+
+	nameEntity := valRef.Type().Name()
+
+	for i := 0; i < valRef.NumField(); i++ {
+		typeField := valRef.Type().Field(i)
+		tag := typeField.Tag
+
+		key := fmt.Sprintf("%s:%d:%s", nameEntity, id, tag.Get("redis"))
+
+		redValue := s.Get(key)
+
+		switch typeField.Type.String() {
+		case "string":
+			rv.Elem().Field(i).SetString(redValue.Val())
+		case "int":
+			val, _ := redValue.Int64()
+			rv.Elem().Field(i).SetInt(val)
+		}
+
+		fmt.Println(key)
+	}
+
+	reflect.Indirect(rv)
+	return rv.Interface()
+}
